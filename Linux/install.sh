681e7d202829aae3e8cf03247e3353e5cb533e4d
BuriKizilkaya <burak.kizilkaya@outlook.com>
2022-12-28 00:46:50 +0100

    Update zshrc

--- Linux/install.sh
+++ Linux/install.sh
@@ -19,18 +19,35 @@ warn() {
   printf "%s[*] %s%s\n" "$(tput setaf 3)" "$1" "$reset_color"
 }
 
+
+info "### Install packages ###"
+sudo apt update
+sudo apt install -y zsh stow
+
+
 # add zsh as a login shell
 info "### Configure Shell ###"
 command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh) $USER
+
+# setup oh my zsh
+info "Installing oh my zsh..."
+if [[ ! -f ~/.zshrc ]]; then
+    ZSH=~/.oh-my-zsh ZSH_DISABLE_COMPFIX=true sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    chmod 744 ~/.oh-my-zsh/oh-my-zsh.sh
+else
+    warn "oh-my-zsh already installed"
+fi
+
+# plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || true
+
 
 info "### Stow pacakges to ###"
 target_dir=${1:-$HOME}
 declare -a stow_folders=("zsh" "git")
 for stow_folder in ${stow_folders[@]}
 do
-    info "Stow ${stow_folder} to ${target_dir}..."
+    info "Stow ${stow_folder} to '${target_dir}'"
     stow -v ${stow_folder} -t ${target_dir}
 done
-
-
